8fbf5a2918a6c0ef1f99e39b810875e0b4cc7dd4
Jim Crossley <jim@jimbook.local>
2010-05-23 14:50:50 -0400

    Virgin rails (2.3.5) app for Cooling Tower Jr  with tmp and log ignored

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,15 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying cookie session data integrity.
+# If you change this key, all old sessions will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.session = {
+  :key         => '_ctjr_session',
+  :secret      => '62d2e8425f8eb1625fc05b0e83d38fea59a847222ec280de311ffb33b898dd7289c696468ea81714ba7e5377169d46c08403ed7747615c8cf91bf6b7b7efddbf'
+}
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+# ActionController::Base.session_store = :active_record_store
