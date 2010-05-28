2da85855593b695d2fc7bea3609eaa3f34e1835c
Jim Crossley <jim@jimbook.local>
2010-05-28 16:37:52 -0400

    Forcing version of net/ssh to 2.0.22
    The "missing public key" bug has been fixed in the latest release.

--- config/environment.rb
+++ config/environment.rb
@@ -20,6 +20,7 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem "aws"
+  config.gem "net-ssh", :version => '2.0.22', :lib => "net/ssh"
   config.gem "net-scp", :lib => "net/scp"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
