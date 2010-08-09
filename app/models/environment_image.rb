dd6ff0f4a6b93ec392291d1303ead43278520a45
Ben Browning <bbrownin@redhat.com>
2010-08-09 17:13:54 -0400

    Assign instances a random cloud id when created

--- app/models/environment_image.rb
+++ app/models/environment_image.rb
@@ -6,10 +6,14 @@ class EnvironmentImage < ActiveRecord::Base
     instance = Instance.new(:image_id => image.id,
                             :environment_id => self.environment_id,
                             :name => "#{image.name} ##{instance_number}",
-                            :cloud_id => "I-80297",
+                            :cloud_id => random_cloud_id,
                             :hardware_profile => hardware_profile,
                             :status => 'running',
                             :public_dns => 'ec2-72-44-82-93.z-2.1-compute.amazonaws.com')
     instance.save!
+  end
+
+  def random_cloud_id
+    "i-#{(1000000000 + rand(3000000000)).to_s(16)}"
   end
 end
