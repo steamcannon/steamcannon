bf47876103db0e8ce0b82b53eda0839cae6a9cd0
Ben Browning <bbrownin@redhat.com>
2010-08-19 12:40:56 -0400

    Initial stab at loading Platforms from YAML (COOLINGTOWER-29)

--- app/models/platform.rb
+++ app/models/platform.rb
@@ -1,7 +1,56 @@
 class Platform < ActiveRecord::Base
   has_many :platform_versions
 
+  accepts_nested_attributes_for :platform_versions
+
   def to_s
     name
+  end
+
+  # Create new Platform(s) from the given YAML file
+  #
+  # This is only meant to be used for creating new platforms, not for
+  # updating existing ones. An example of the expected YAML file syntax:
+  #
+  # ---
+  # platforms:
+  #   - name: Test Platform
+  #     platform_versions:
+  #       - version_number: 123
+  #         images:
+  #           - name: Test Image 123
+  #             cloud_id: ami_123
+  #             role: frontend
+  #           - name: Test Image 234
+  #             cloud_id: ami_234
+  #             role: backend
+  #       - version_number: 234
+  #         images:
+  #           - cloud_id: ami_123
+  #
+  # When specifying images for a PlatformVersion, the cloud_id is
+  # required. If an Image already exists with the same cloud_id then
+  # it will be used, otherwise a new Image will be created and any
+  # extra Image attributes you pass will get used for the creation.
+  #
+  def self.create_from_yaml(file_path)
+    yaml = YAML::load_file(file_path)
+    yaml['platforms'].each do |platform_yaml|
+      platform_versions = platform_yaml.delete('platform_versions')
+      platform = Platform.new(platform_yaml)
+      unless platform_versions.nil?
+        platform_versions.each do |version_yaml|
+          images = version_yaml.delete('images')
+          version = PlatformVersion.new(version_yaml)
+          unless images.nil?
+            images.each do |image_yaml|
+              version.images << Image.find_or_create_by_cloud_id(image_yaml)
+            end
+          end
+          platform.platform_versions << version
+        end
+      end
+      platform.save!
+    end
   end
 end
--- spec/models/platform_spec.rb
+++ spec/models/platform_spec.rb
@@ -22,4 +22,37 @@ describe Platform do
   it "should return its name as to_s" do
     Platform.new(:name => "test").to_s.should eql("test")
   end
+
+  describe "yaml loading" do
+    before(:each) do
+      @yaml_path = File.join(RAILS_ROOT, 'spec/fixtures/platforms')
+    end
+
+    it "should support simple platforms" do
+      Platform.create_from_yaml(File.join(@yaml_path, 'simple_platform.yml'))
+      Platform.find_by_name("Test Platform").should_not be_nil
+      # platform = Platform.find_by_name("Test Platform")
+      # platform.should_not be_nil
+      # platform.platform_versions.should_not be_empty
+      # version = PlatformVersion.find_by_version_number(123)
+      # version.should_not be_nil
+      # version.images.should_not be_empty
+      # Image.find_all_by_cloud_id('ami_123').count.should be(1)
+    end
+
+    it "should support platform versions" do
+      Platform.create_from_yaml(File.join(@yaml_path, 'platform_versions_support.yml'))
+      platform = Platform.find_by_name("Test Platform")
+      platform.platform_versions.should_not be_empty
+      PlatformVersion.find_by_version_number(123).should_not be_nil
+    end
+
+    it "should support images" do
+      Platform.create_from_yaml(File.join(@yaml_path, 'images_support.yml'))
+      version = PlatformVersion.find_by_version_number(123)
+      version.should_not be_nil
+      version.images.should_not be_empty
+      Image.find_all_by_cloud_id('ami_123').count.should be(1)
+    end
+  end
 end
