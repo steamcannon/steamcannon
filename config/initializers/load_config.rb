abf0505dd941c36537309768b33dd031be8c7b7b
Ben Browning <bbrownin@redhat.com>
2010-08-20 07:56:49 -0400

    Make the cooling-tower.yml optional so we can run specs w/o it

--- config/initializers/load_config.rb
+++ config/initializers/load_config.rb
@@ -1,5 +1,2 @@
 
-APP_CONFIG = YAML.load_file("#{RAILS_ROOT}/config/cooling-tower.yml")
-
-# We shouldn't mess with instances we're not configured to care about
-APP_CONFIG['image_ids'] = APP_CONFIG.entries.select{|x,y| x.ends_with? 'image_id'}.map{|x,y| y}
+APP_CONFIG = YAML.load_file("#{RAILS_ROOT}/config/cooling-tower.yml") rescue {}
