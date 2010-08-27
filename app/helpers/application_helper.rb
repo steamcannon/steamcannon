0e87b0acada4c0e3e8fdaa3398ebf37f1bf564ef
Tobias Crawley <tcrawley@gmail.com>
2010-08-27 16:59:16 -0400

    Don't put the user type as a class on body since we don't use it to hide content anymore.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
   def dom_class_for_body
-    %(#{params[:controller]}_controller #{params[:action]}_action #{current_user and current_user.superuser? ? 'superuser' : 'account_user'})
+    %(#{params[:controller]}_controller #{params[:action]}_action)
   end
 
   def content_for_superuser(text = nil, &block)
