dc9e25e00b1ca516a2fbaf9b666b8c7d82e12b2a
Tobias Crawley <tcrawley@gmail.com>
2010-08-27 16:56:13 -0400

    Added content_for_superuser to handle conditional output in shared views.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,9 @@ module ApplicationHelper
   def dom_class_for_body
     %(#{params[:controller]}_controller #{params[:action]}_action #{current_user and current_user.superuser? ? 'superuser' : 'account_user'})
   end
+
+  def content_for_superuser(text = nil, &block)
+    raise ArgumentError.new("Don't supply both text and a block") if text and block_given?
+    concat(text || capture(&block)) if current_user.superuser?
+  end
 end
