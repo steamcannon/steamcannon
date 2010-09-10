eae24ffceedc7e35d52e61e65279497ad9840668
Tobias Crawley <tcrawley@gmail.com>
2010-09-10 17:15:39 -0400

    Fixed failing job test, and added a few more.

--- spec/jobs/instance_watcher_spec.rb
+++ spec/jobs/instance_watcher_spec.rb
@@ -23,18 +23,38 @@ describe InstanceWatcher do
     @instance_watcher = InstanceWatcher.new
   end
 
-  it "should update starting and terminating instances" do
+  it "should update starting, configuring, verifying, and terminating instances" do
     @instance_watcher.should_receive(:update_starting)
+    @instance_watcher.should_receive(:update_configuring)
+    @instance_watcher.should_receive(:update_verifying)
     @instance_watcher.should_receive(:update_terminating)
     @instance_watcher.run
   end
 
-  it "should transition each starting instance to running" do
+  it "should transition each starting instance to configuring" do
     instance = mock_model(Instance)
-    instance.should_receive(:run!)
+    instance.should_receive(:configure!)
     Instance.stub!(:starting).and_return([instance])
     @instance_watcher.update_starting
   end
+
+  it "should attempt to configure any configuring instances" do
+    instance = mock_model(Instance)
+    instance.should_receive(:configure_agent)
+    Instance.stub!(:configuring).and_return([instance])
+    @instance_watcher.update_configuring
+  end
+  
+  it "should attempt to verify any verifying instances (PENDING: agent needs to be able to configure itself)"
+    
+  it "should transition each configuring instance to running (until agent can configure itself)" do
+    instance = mock_model(Instance)
+    #instance.should_receive(:verify_agent)
+    instance.should_receive(:run!)
+    Instance.stub!(:verifying).and_return([instance])
+    @instance_watcher.update_verifying
+  end
+
 
   it "should transition each terminating instance to stopped" do
     instance = mock_model(Instance)
