653ea98afede5f67eef4c805005f1a82516f3508
Tobias Crawley <tcrawley@gmail.com>
2010-09-10 16:14:37 -0400

    Add rest-client gem to environment.rb.

--- config/environment.rb
+++ config/environment.rb
@@ -30,7 +30,8 @@ Rails::Initializer.run do |config|
   config.gem "paperclip", :version => "2.3.3"
   config.gem "simple-navigation", :version => "2.6.0"
   config.gem "aasm", :version => "2.1.5"
-
+  config.gem "rest-client", :version => '1.6.0'
+  
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
