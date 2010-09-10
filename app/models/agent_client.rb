6f361109650768a86317975500b68270f98041c8
Tobias Crawley <tcrawley@gmail.com>
2010-09-10 17:28:09 -0400

    Let's see if installing json does the trick with CI.

--- app/models/agent_client.rb
+++ app/models/agent_client.rb
@@ -16,8 +16,6 @@
 # Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
 # 02110-1301 USA, or see the FSF site: http://www.fsf.org.
 
-require 'rest-client'
-
 class AgentClient
   AGENT_PORT = 7575
 
--- config/environment.rb
+++ config/environment.rb
@@ -31,6 +31,7 @@ Rails::Initializer.run do |config|
   config.gem "simple-navigation", :version => "2.6.0"
   config.gem "aasm", :version => "2.1.5"
   config.gem "rest-client", :version => '1.6.0'
+  config.gem "json_pure", :version => '1.4.6', :lib => 'json'
   
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
