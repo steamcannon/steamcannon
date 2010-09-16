ffcd7197d5f53ec5a3c954032ca303cd9a964a63
Lance <lance@shovelpunks.com>
2010-09-16 16:42:58 -0400

    Switch to postgresql instead of sqlite

---
+++ bin/create_db.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+echo "Creating steamcannon databases"
+echo "CREATE DATABASE steamcannon_dev" | psql 
+echo "CREATE DATABASE steamcannon_test" | psql 
+echo "CREATE DATABASE steamcannon_production" | psql 
+
+echo "Creating steamcannon user"
+echo "CREATE USER steamcannon WITH PASSWORD 'steamcannon'" | psql
+echo "GRANT ALL ON DATABASE steamcannon_dev TO steamcannon" | psql
+echo "GRANT ALL ON DATABASE steamcannon_test TO steamcannon" | psql
+
+echo "Granting steamcannon user all privs on databases"
+echo "GRANT ALL ON DATABASE steamcannon_dev TO steamcannon" | psql
+echo "GRANT ALL ON DATABASE steamcannon_test TO steamcannon" | psql
+echo "GRANT ALL ON DATABASE steamcannon_production TO steamcannon" | psql
+
+
+rake db:setup
