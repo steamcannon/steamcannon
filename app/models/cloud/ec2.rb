7cfffeecb2439022179a591dc846fe6a333a53f5
Ben Browning <bbrownin@redhat.com>
2010-09-28 13:07:45 -0400

    Switch multicast bucket creation from S3 -> AWS gems

--- app/models/cloud/ec2.rb
+++ app/models/cloud/ec2.rb
@@ -18,6 +18,7 @@
 
 module Cloud
   class Ec2
+    extend ActiveSupport::Memoizable
 
     def initialize(instance)
       @instance = instance
@@ -86,7 +87,7 @@ module Cloud
       access_key = @instance.cloud.cloud_username
       secret_access_key = @instance.cloud.cloud_password
       environment = @instance.environment
-      s3_bucket = find_or_create_multicast_bucket(environment.user)
+      s3_bucket = multicast_bucket(environment.user)
       s3_resource = "Environment#{environment.id}/instance#{@instance.id}"
       expires_at = @instance.created_at + 1.year
 
@@ -98,23 +99,19 @@ module Cloud
       S3::Signature.generate_temporary_url(options)
     end
 
-    def find_or_create_multicast_bucket(user)
-      unless @bucket_name
-        bucket_suffix = Digest::SHA1.hexdigest(user.cloud_username)
-        # "_" required in bucket name to workaround a bug in S3 gem
-        @bucket_name = "SteamCannonEnvironments_#{bucket_suffix}"
+    def multicast_bucket(user)
+      bucket_suffix = Digest::SHA1.hexdigest(user.cloud_username)
+      bucket_name = "SteamCannonEnvironments_#{bucket_suffix}"
 
-        service = S3::Service.new(:access_key_id => user.cloud_username,
-                                  :secret_access_key => user.cloud_password)
+      s3 = Aws::S3.new(user.cloud_username,
+                       user.cloud_password,
+                       :multi_thread => true)
 
-        # Ensure our bucket exists and has correct permissions
-        bucket = service.buckets.build(@bucket_name)
-        # Unfortunately, the S3 gem does some funkiness with headers
-        # here and it has to be :x_amz_acl instead of 'x-amz-acl' like above
-        bucket.save(:headers => {:x_amz_acl => 'public-read'})
-      end
-      @bucket_name
+      # Ensure our bucket exists and has correct permissions
+      bucket = Aws::S3::Bucket.create(s3, bucket_name, true, 'public-read')
+      bucket_name
     end
+    memoize :multicast_bucket
 
     def ensure_security_group(options)
       user = options[:user]
