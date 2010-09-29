b0c4db0a9ab600eaa722de93d2477c7d09b4b98f
Lance <lance@shovelpunks.com>
2010-09-29 18:17:43 -0400

    Actually add the InstanceReaper class to the repo

---
+++ app/jobs/instance_reaper.rb
@@ -0,0 +1,27 @@
+# Copyright 2010 Red Hat, Inc.
+#
+# This is free software; you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as
+# published by the Free Software Foundation; either version 3 of
+# the License, or (at your option) any later version.
+#
+# This software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this software; if not, write to the Free
+# Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+# 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+
+
+class InstanceReaper
+
+  def run
+    # Check all running instances and ensure that they are still running
+    # otherwise make them unavailable
+    Instance.running.each { |i| i.unavailable! if (i.cloud.nil? || i.cloud_instance.nil?) }
+  end
+
+end
