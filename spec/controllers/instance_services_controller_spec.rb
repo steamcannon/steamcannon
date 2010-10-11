069d642ba7a4d6382c821f6dd6665c01455e4836
Ben Browning <bbrownin@redhat.com>
2010-10-11 11:22:47 -0400

    tests for instance_services controller (STEAM-126)

--- spec/controllers/instance_services_controller_spec.rb
+++ spec/controllers/instance_services_controller_spec.rb
@@ -14,15 +14,41 @@ describe InstanceServicesController do
       @instance_service = Factory(:instance_service)
       Environment.stub!(:find).with("123").and_return(@environment)
       Instance.stub!(:find).with("234").and_return(@instance)
-      @instance.stub_chain(:instance_services, :find).and_return(InstanceService)
+      @instance.stub!(:instance_services).and_return(InstanceService)
       InstanceService.stub!(:find).with("1").and_return(@instance_service)
-      controller.stub!(:log_ids).and_return([])
-      controller.stub!(:tail_response).and_return({})
     end
 
     it "should be successful" do
+      controller.stub!(:log_ids).and_return([])
       get :logs, :environment_id => 123, :instance_id => 234, :id => 1
       response.should be_success
+    end
+
+    it "should retrieve valid log ids from agent client" do
+      agent_client = mock('agent_client')
+      @instance_service.should_receive(:agent_client).and_return(agent_client)
+      agent_client.should_receive(:logs).and_return(['log_id'])
+      get :logs, :environment_id => 123, :instance_id => 234, :id => 1
+    end
+
+    context "js format" do
+      it "should be successful" do
+        controller.stub!(:tail_response).and_return({})
+        xhr :get, :logs, :environment_id => 123, :instance_id => 234, :id => 1
+        response.should be_success
+      end
+
+      it "should generate tail response" do
+        controller.should_receive(:tail_response).and_return({:offset => 0})
+        xhr :get, :logs, :environment_id => 123, :instance_id => 234, :id => 1
+      end
+
+      it "should retrieve tail response from agent client" do
+        agent_client = mock('agent_client')
+        @instance_service.should_receive(:agent_client).and_return(agent_client)
+        agent_client.should_receive(:fetch_log).and_return({})
+        xhr :get, :logs, :environment_id => 123, :instance_id => 234, :id => 1
+      end
     end
   end
 end
