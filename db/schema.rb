672552898c07375517f15e1901ed97844d496e21
Bob McWhirter <bob@mcwhirter.org>
2010-10-14 12:44:26 -0400

    Tweaking header actions.

--- db/schema.rb
+++ db/schema.rb
@@ -101,6 +101,13 @@ ActiveRecord::Schema.define(:version => 20101014133702) do
     t.boolean  "can_scale_out",           :default => false
   end
 
+  create_table "instance_deployments", :force => true do |t|
+    t.integer  "instance_id"
+    t.integer  "deployment_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "instance_services", :force => true do |t|
     t.integer  "instance_id"
     t.integer  "service_id"
