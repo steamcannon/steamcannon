5dad7450bdfb1e53273ac6688dd5e83605db21f0
Bob McWhirter <bob@mcwhirter.org>
2010-10-15 10:25:09 -0400

    Yellow artifact name, for Jimbo.

--- db/schema.rb
+++ db/schema.rb
@@ -102,6 +102,13 @@ ActiveRecord::Schema.define(:version => 20101014215254) do
     t.boolean  "can_scale_out",           :default => false
   end
 
+  create_table "instance_deployments", :force => true do |t|
+    t.integer  "instance_id"
+    t.integer  "deployment_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "instance_services", :force => true do |t|
     t.integer  "instance_id"
     t.integer  "service_id"
@@ -115,7 +122,6 @@ ActiveRecord::Schema.define(:version => 20101014215254) do
   create_table "instances", :force => true do |t|
     t.integer  "environment_id"
     t.integer  "image_id"
-    t.string   "name"
     t.string   "cloud_id"
     t.string   "hardware_profile"
     t.string   "current_state"
