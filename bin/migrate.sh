1879d9a6f8c32f934adde7ce38b7d699a9987839
Lance <lance@shovelpunks.com>
2010-10-28 12:26:36 -0400

    Add migration script to bin

---
+++ bin/migrate.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+TARGET=$1
+
+if [ "." == $TARGET. ] ; then
+  echo "Usage: migrate.sh <target>"
+  exit 1
+fi
+
+su postgres -c "pg_dump steamcannon_production | psql -h $TARGET steamcannon_production -U steamcannon"
