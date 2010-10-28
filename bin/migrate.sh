52e64ce751bcbdeb3c42b71aea56b3a58b91c81b
Lance <lance@shovelpunks.com>
2010-10-28 16:36:45 -0400

    Make migration destructive with a rake db:migrate. Add some helpful instructive text to the output if the migrationi is successful

--- bin/migrate.sh
+++ bin/migrate.sh
@@ -7,4 +7,14 @@ if [ "." == $TARGET. ] ; then
   exit 1
 fi
 
-su postgres -c 'pg_dump steamcannon_production'  | psql -h $TARGET steamcannon_production -U steamcannon
+su postgres -c 'pg_dump -c steamcannon_production'  | psql -h $TARGET steamcannon_production -U steamcannon
+
+if [ $? -eq 0 ] ; then
+    echo "Data has been moved to $TARGET"
+    echo "Now you need to ssh to $TARGET and execute the following commands."
+    echo -e "\t# cd /opt/steamcannon"
+    echo -e "\t# RAILS_ENV=production /opt/jruby/bin/jruby -S rake db:migrate"
+    echo -e "\t# RAILS_ENV=production /opt/jruby/bin/jruby -S rake db:seed"
+else
+    echo "Unable to migrate. Sorry!"
+fi
