1534f60c222da5f53a211060b56fd6ffb69c767b
Tobias Crawley <tcrawley@gmail.com>
2010-10-29 09:29:31 -0400

    Hack to see if we can work around STEAM-222 in production.

--- app/tasks/model_task.rb
+++ app/tasks/model_task.rb
@@ -21,10 +21,28 @@ class ModelTask < TorqueBox::Messaging::Task
 
   # FIXME: this won't handle active_record objects in the args. We
   # should fix if we need that.
-  def perform(payload)
+  def perform(payload, retry_count = 0)
     payload[:class_name].constantize.find(payload[:id]).send(payload[:method], *(payload[:args] || []))
+  rescue ActiveRecord::RecordNotFound => ex
+    # use puts to get in to server.log
+    puts "ModelTask#perform: FAILED to find record: #{ex}"
+    if retry_count < 3
+      retry_count += 1
+      puts "ModelTask#perform: retrying after 1 second sleep (retry_count: #{retry_count})"
+      sleep(1)
+      perform(payload, retry_count)
+    else
+      puts "ModelTask#perform: giving up"
+    end
   end
 
+  class << self
+    alias_method :async_orig, :async
+  end
+  def self.fake(id)
+    async_orig(:perform, :method => :nil?, :class_name => 'Deployment', :id => id, :args => [])
+  end
+  
   def self.async(model, method, *args)
     super(:perform, :method => method, :class_name => model.class.name, :id => model.id, :args => args)
   end
