95e8a1b1db7b975308772af3e210e1fe10f3775e
Ben Browning <bbrownin@redhat.com>
2010-12-03 08:33:57 -0500

    For some reason db/schema wasn't up-to-date

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101201114628) do
+ActiveRecord::Schema.define(:version => 20101201123812) do
 
   create_table "account_requests", :force => true do |t|
     t.string   "email"
