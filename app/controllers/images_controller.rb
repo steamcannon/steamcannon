31a62d69ecb7f6d07d7cb00760429f96af6737f9
Ben Browning <bbrownin@redhat.com>
2010-12-13 12:39:56 -0500

    Add missing license header

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,3 +1,21 @@
+#
+# Copyright 2010 Red Hat, Inc.
+#
+# This is free software; you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as
+# published by the Free Software Foundation; either version 3 of
+# the License, or (at your option) any later version.
+#
+# This software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this software; if not, write to the Free
+# Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+# 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+
 class ImagesController < ApplicationController
   def index
     @environment = current_user.environments.find(params[:environment_id])
@@ -8,6 +26,6 @@ class ImagesController < ApplicationController
     @environment = current_user.environments.find(params[:environment_id])
     @image = Image.find(params[:id])
     # It's all complicated like this to scope the DB query to the user instead of just using CloudImage.find_by_cloud_id
-    @cloud_image = @image.cloud_images.flatten.first{|ci|ci.cloud_id == params[:id]} 
+    @cloud_image = @image.cloud_images.flatten.first{|ci|ci.cloud_id == params[:id]}
   end
 end
