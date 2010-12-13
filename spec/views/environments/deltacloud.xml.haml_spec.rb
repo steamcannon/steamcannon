cafa72fd9041876376c0108eb93d881505ff68b3
Tobias Crawley <tcrawley@redhat.com>
2010-12-13 11:01:39 -0500

    Additional cloud_profile changes [STEAM-245]

--- spec/views/environments/deltacloud.xml.haml_spec.rb
+++ spec/views/environments/deltacloud.xml.haml_spec.rb
@@ -24,16 +24,16 @@ describe "/environments/deltacloud.xml.haml" do
   before(:each) do
     @environment = stub_model(Environment)
     assigns[:environment] = @environment
-    @environment.stub_chain(:user, :cloud, :name).and_return('the-cloud')
-    @environment.stub_chain(:user, :cloud, :hardware_profiles_url).and_return('http://test.server/hardware_profiles')
-    @environment.stub_chain(:user, :cloud, :realms_url).and_return('http://test.server/realms')
-    @environment.stub_chain(:user, :cloud, :instance_states_url).and_return('http://test.server/instance_states')
-    @environment.stub_chain(:user, :cloud, :images_url).and_return('http://test.server/images')
-    @environment.stub_chain(:user, :cloud, :instances_url).and_return('http://test.server/instances')
+    @environment.stub_chain(:cloud_profile, :cloud, :name).and_return('the-cloud')
+    @environment.stub_chain(:cloud_profile, :cloud, :hardware_profiles_url).and_return('http://test.server/hardware_profiles')
+    @environment.stub_chain(:cloud_profile, :cloud, :realms_url).and_return('http://test.server/realms')
+    @environment.stub_chain(:cloud_profile, :cloud, :instance_states_url).and_return('http://test.server/instance_states')
+    @environment.stub_chain(:cloud_profile, :cloud, :images_url).and_return('http://test.server/images')
+    @environment.stub_chain(:cloud_profile, :cloud, :instances_url).and_return('http://test.server/instances')
   end
 
   it "renders the DeltaCloud API endpoint for the enironment" do
-    @environment.user.cloud.should_receive(:name).and_return('the-cloud')
+    @environment.cloud.should_receive(:name).and_return('the-cloud')
     render
     response.should have_tag("api[driver=?]", 'the-cloud') do
       with_tag("link[rel=?][href=?][proxy-for=?]", "hardware_profiles", hardware_profiles_url, @environment.cloud.hardware_profiles_url)
