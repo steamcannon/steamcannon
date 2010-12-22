d14ce96db1d4ac102debf9d00175f1bd2fbbbcbe
Tobias Crawley <tcrawley@redhat.com>
2010-12-22 13:45:58 -0500

    Fixes to migrations so they will work with a real dataset.

--- db/migrate/20101123125521_put_existing_storage_volumes_in_proper_state.rb
+++ db/migrate/20101123125521_put_existing_storage_volumes_in_proper_state.rb
@@ -1,16 +1,14 @@
 class StorageVolume < ActiveRecord::Base
+  belongs_to :instance
 end
 
 class PutExistingStorageVolumesInProperState < ActiveRecord::Migration
   def self.up
     StorageVolume.all.each do |sv|
-      state = nil
-      state = 'attached' if sv.cloud_volume_is_attached?
-      state ||= 'available' if sv.cloud_volume_is_available?
-      state ||= 'not_found' unless sv.cloud_volume_exists?
-      unless state
-        puts ">> StorageVolume##{sv.id} is not in a known state!"
-        exit
+      if sv.instance && sv.instance.running?
+        state = 'attached'
+      else
+        state = 'available' 
       end
       puts ">> putting StorageVolume##{sv.id} in :#{state} state"
       sv.update_attribute(:current_state, state)
--- db/migrate/20101209143112_update_realm_on_running_environments.rb
+++ db/migrate/20101209143112_update_realm_on_running_environments.rb
@@ -1,4 +1,5 @@
 class Environment < ActiveRecord::Base
+  belongs_to :user
 end
 
 class UpdateRealmOnRunningEnvironments < ActiveRecord::Migration
