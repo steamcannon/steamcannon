28e44c302b2054cc801cbe55968634743b83be1c
Lance <lance@shovelpunks.com>
2011-01-04 18:15:12 -0500

    List deployments in the API

--- app/controllers/deployments_controller.rb
+++ app/controllers/deployments_controller.rb
@@ -29,7 +29,7 @@ class DeploymentsController < ApplicationController
 
     respond_to do |format|
       format.html # index.html.erb
-      format.xml  { render :xml => @deployments }
+      format.xml  # index.xml.haml
     end
   end
 
