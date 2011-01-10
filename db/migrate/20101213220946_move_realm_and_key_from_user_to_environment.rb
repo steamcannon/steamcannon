4e7ea8155b8d1d60a82f70f31b4717a60e0eb404
Tobias Crawley <tcrawley@redhat.com>
2011-01-10 11:30:02 -0500

    Don't run validations on the environment.

--- db/migrate/20101213220946_move_realm_and_key_from_user_to_environment.rb
+++ db/migrate/20101213220946_move_realm_and_key_from_user_to_environment.rb
@@ -1,10 +1,11 @@
+
 class MoveRealmAndKeyFromUserToEnvironment < ActiveRecord::Migration
   def self.up
     User.all.each do |user|
       user.environments.each do |env|
         env.realm ||= user.default_realm
         env.ssh_key_name = user.ssh_key_name
-        env.save
+        env.save(false)
       end
     end
   end
