62f52410b6f94ba58ef12a65ca535ad1652ef05e
Lance <lance@shovelpunks.com>
2011-01-10 14:05:58 -0500

    Clean up API client

--- lib/steamcannon/client.rb
+++ lib/steamcannon/client.rb
@@ -4,10 +4,13 @@ require 'base64'
 
 module SteamCannon
   module API
-    class AbstractApi
+    class Connector
       attr_accessor :login, :pass
-      @data = {}
-  
+
+      def initialize(login, pass)
+        @login, @pass = login, pass
+      end
+
       def default_headers
         auth_header = "Basic "+Base64.encode64("#{@login}:#{@pass}")
         auth_header.gsub!("\n", "")
@@ -21,6 +24,11 @@ module SteamCannon
         XmlSimple.xml_in(RestClient.send(:get, url.to_s, default_headers).dup, options)
       end
 
+    end
+
+    class AbstractApi
+      @data = {}
+  
       protected
       def method_missing(m, *args, &block)
         value = @data[m.to_s]
@@ -31,15 +39,14 @@ module SteamCannon
     class Client < AbstractApi
 
       def initialize(url, login, pass)
-        @endpoint   = URI.parse(url)
-        @login      = login
-        @pass       = pass
         @url        = url
+        @endpoint   = URI.parse(@url)
+        @connector  = Connector.new(login, pass)
         fetch_endpoint
       end
 
       def environments
-        @environments ||= request(@environments_url)['environment'].collect{|e|Environment.new(self, e)}
+        @environments ||= fetch_environments
       end
 
       def artifacts
@@ -52,21 +59,24 @@ module SteamCannon
 
       protected
       def fetch_endpoint
-        response            = request(@endpoint)
+        response            = @connector.request(@endpoint)
         @environments_url   = response['link']['environments']['href']
         @artifacts_url      = response['link']['artifacts']['href']
         @cloud_profiles_url = response['link']['cloud_profiles']['href']
       end
 
+      def fetch_environments
+        @connector.request(@environments_url)['environment'].collect{|e|Environment.new(@connector, e)}
+      end
+
       def fetch_artifacts
-        request(@artifacts_url)['artifact'].collect { |artifact| @artifacts << artifact }
+        @connector.request(@artifacts_url)['artifact'].collect { |a| Artifact.new(@connector, a) }
       end
     end
 
     class Environment < AbstractApi
-      def initialize(client, data)
-        @login = client.login
-        @pass = client.pass
+      def initialize(connector, data)
+        @connector = connector
         @data = data
       end
 
@@ -76,7 +86,7 @@ module SteamCannon
   
       def deployments
         unless @deployments
-          response = request(@data['deployments'][0]['href'])['deployment']
+          response = @connector.request(@data['deployments'][0]['href'])['deployment']
           @deployments = response.nil? ? [] : response.collect{|d|Deployment.new(self, d)}
         end
         @deployments 
@@ -84,10 +94,22 @@ module SteamCannon
     end
 
     class Deployment  < AbstractApi
-      def initialize(client, data)
-        @login = client.login
-        @pass = client.pass
+      def initialize(connector, data)
+        @connector = connector
         @data = data
+      end
+    end
+
+    class Artifact < AbstractApi
+      def initialize(connector, data)
+        @connector = connector
+        @data = data
+      end
+
+      def versions
+        unless @versions
+          @versions = @data['artifact_version'].collect{|v| v['href']}
+        end
       end
     end
   end
