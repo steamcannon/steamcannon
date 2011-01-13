f86aa639e905fd8bec74160dc6ff06ec8ed5336f
Tobias Crawley <tcrawley@redhat.com>
2011-01-13 16:59:52 -0500

    Fixed failing platform controller tests.

--- spec/controllers/platforms_controller_spec.rb
+++ spec/controllers/platforms_controller_spec.rb
@@ -20,33 +20,45 @@ require 'spec_helper'
 
 describe PlatformsController do
 
-  describe "GET platforms/new" do
-    describe "when not logged in" do
-      before(:each) { logout }
-      it "should require login" do
-        get :new
-        response.should redirect_to(new_user_session_path)
-      end
+  describe "when not logged in" do
+    before(:each) { logout }
+    it "should require login" do
+      get :new
+      response.should redirect_to(new_user_session_path)
+    end
+  end
+
+  describe "when logged in" do
+    before(:each) do
+      @superuser = Factory.build(:superuser)
+      @account_user = Factory.build(:user)
     end
 
-    describe "when logged in" do
-      before(:each) do
-        @superuser = Factory.build(:superuser)
-        @account_user = Factory.build(:user)
-      end
-
+    describe "GET new" do
       it "should redirect to login if user is not superuser" do
         login_with_user(@account_user)
-        get :index
+        get :new
         response.should be_redirect
       end
 
       it "should require superuser" do
         login_with_user(@superuser)
+        get :new
+        response.should render_template(:new)
+      end
+    end
+
+
+    describe "GET index" do
+      it "should not require superuser" do
+        login_with_user(@account_user)
         get :index
         response.should render_template(:index)
       end
     end
+    
   end
-
+  
 end
+
+
