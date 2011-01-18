245b034454be7112dbff237f89051f85760a1f82
Lance <lance@shovelpunks.com>
2011-01-18 10:58:19 -0500

    Make artifact_versions a proper collection in <artifact> to facilitate upload of new versions

--- lib/steamcannon/api/artifact.rb
+++ lib/steamcannon/api/artifact.rb
@@ -8,9 +8,13 @@ module SteamCannon
         end
       end
 
+      def create_version(data)
+        puts "This part isn't done yet - but when it is, we'll upload #{data}"
+      end
+
       protected
       def parse_versions
-        @data['artifact_version'].collect{|v| ArtifactVersion.new(@connector, v)}
+        @data['artifact_versions'][0]['artifact_version'].collect{|v| ArtifactVersion.new(@connector, v)}
       end
     end
   end
